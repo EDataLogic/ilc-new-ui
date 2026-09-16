11817ab6570fdf111196e14cc515894297daadef
Sumit <sumit.kothari@iliveconnect.com>
2026-09-17 00:13:32 +0530

    deploy static

--- src/main.jsx
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import ILiveConnect from './ILiveConnect.jsx';
-
-ReactDOM.createRoot(document.getElementById('root')).render(<ILiveConnect />);
